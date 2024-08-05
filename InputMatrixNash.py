188bda79bc3e3783fa710ce7ee5ff81c9f67a576
Cheemshoang <123573915+Cheemshoang@users.noreply.github.com>
2024-08-05 22:54:39 +0700

    Update InputMatrixNash.py

--- InputMatrixNash.py
+++ InputMatrixNash.py
@@ -8,8 +8,11 @@ def input_matrix():
         matrix.append(list(map(float, row.split())))
 
     return matrix
-    
+
+
+
 def Nash(matrix_list):
+  output = []
   matrix_np = np.array(matrix_list)
   min_value = np.min(matrix_np)
   matrix_np = matrix_np - min_value + 1
@@ -39,12 +42,14 @@ def Nash(matrix_list):
 
 
   if result.success:
-    w = 1/result.fun
-    array1 = w * result.x
-    w = w + min_value - 1
-    print("v_1 = ", w)
-    print("X_0", array1)
-  else:  print("No solution found. The problem might be infeasible or unbounded.")
+    w1 = 1/result.fun
+    array1 = w1 * result.x
+    w1 = w1 + min_value - 1
+    output.append(w1)
+    output.append(array1)
+  else:  
+    print("No solution found. The problem might be infeasible or unbounded.")
+    return 
 
 
   #P2
@@ -76,14 +81,16 @@ def Nash(matrix_list):
 
 
   if result.success:
-    w = -1/result.fun
-    array2 = w * result.x
-    w = w + min_value - 1
-    print("v_2 = ", w)
-    print("Y_0" , array2)
+    w2 = -1/result.fun
+    array2 = w2 * result.x
+    w2 = w2 + min_value - 1
+    output.append(w2)
+    output.append(array2)
   else:
     print("No solution found. The problem might be infeasible or unbounded.")
-      
+    return 
+  return output
+
 #Main
 matrix = input_matrix()
 Nash(matrix)
